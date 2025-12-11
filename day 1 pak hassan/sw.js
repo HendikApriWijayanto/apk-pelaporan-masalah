fa38141b2984209d0859e2fd240d208a31dd4dee
BlondedOcean <hendikapriw20042004@gmail.com>
2025-12-11 11:50:49 +0700

    stfu

---
+++ day 1 pak hassan/sw.js
@@ -0,0 +1,4 @@
+self.addEventListener ("install", event => { console.log("Service Worker installed!");
+}) ;
+self.addEventListener ("activate", event => { console.log("Service Worker activated!");
+}) ;
